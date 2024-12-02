0096bc8c45d6eb12a60eff0578c07a23de364245
corentinjs <corentin.janson@epitech.eu>
2024-12-02 11:39:52 +0100

    Update dotfiles nvim & tmux

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -13,7 +13,7 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "gopls" },
+				ensure_installed = { "lua_ls", "gopls", "docker_compose_language_service", "dockerls", "ts_ls" },
 			})
 		end,
 	},
@@ -29,6 +29,25 @@ return {
 				capabilities = capabilities,
 				cmd = { "gopls" },
 				filetypes = { "go", "gomod", "gowork", "gotmpl" },
+			})
+			lspconfig.docker_compose_language_service.setup({
+				cmd = { "docker-compose-langserver", "--stdio" },
+				filetypes = { "yaml.docker-compose" },
+			})
+			lspconfig.dockerls.setup({
+				cmd = { "docker-langserver", "--stdio" },
+				filetypes = { "dockerfile" },
+			})
+			lspconfig.ts_ls.setup({
+				cmd = { "typescript-language-server", "--stdio" },
+				filetypes = {
+					"javascript",
+					"javascriptreact",
+					"javascript.jsx",
+					"typescript",
+					"typescriptreact",
+					"typescript.tsx",
+				},
 			})
 			vim.keymap.set("n", "I", vim.lsp.buf.hover, {})
 		end,
